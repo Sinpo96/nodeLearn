af1d6fa67e31fe0bcc23db034a9183f1e01cdd44
Sinpo96 <sinpo96@163.com>
2018-12-27 18:22:47 +0800

    2018年12月27日18:22:45

---
+++ Node/02_url.js
@@ -0,0 +1,11 @@
+const url = require('url');
+
+const http = require('http');
+
+http.createServer((req, res) => {
+    res.writeHead(200, {
+        "Content-Type": "text/html"
+    });
+    res.write(req.url);
+    res.end();
+}).listen(3000);
